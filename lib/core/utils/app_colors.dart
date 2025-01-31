d2a1c5e19446a6c2e434819f9d652d37d75454d9
Mohiigul <irodafozilova04@gmail.com>
2025-01-31 11:46:57 +0500

    Add SplashScreen

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const mainColor = Color(0xFF5E17EB);
+  static const backgroundColor = Color(0xFF112095);
+  static const lightBlue = Color(0xFF57A2DD);
+  static const whiteColor = Colors.white;
+  static const darkBlue = Color(0xFF0A2060);
+}
