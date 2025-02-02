e8849102147cf4b0ded2ca8dc234a9d14ca3afea
Mohiigul <irodafozilova04@gmail.com>
2025-02-02 22:16:26 +0500

    changed main

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-<<<<<<< HEAD
-=======
+
+
 import 'package:quran_app/core/di/service_locator.dart';
 import 'package:quran_app/core/routes/app_router.dart';
 
->>>>>>> 646393a4bcb8bec197f2534f4cfe4e99530ec072
-import 'package:quran_app/features/surahs/presentation/screens/surahs_screen.dart';
+
 
 
 
@@ -36,33 +35,30 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
 
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => getIt<SurahInfoBloc>()
-            ..add(
-              FetchSurahInfo(
-                surahNumber: 1,
-              ),
-            ),
-        ),
-      ],
-      child: ScreenUtilInit(
-        designSize: Size(360, 690),
-        child: MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          routerConfig: AppRouter.router,
-          title: 'Quran App',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
+    // return MultiBlocProvider(
+    //   providers: [
+    //     BlocProvider(
+    //       create: (context) => getIt<SurahInfoBloc>()
+    //         ..add(
+    //           FetchSurahInfo(
+    //             surahNumber: 1,
+    //           ),
+    //         ),
+    //     ),
+    //   ],
+    //   child: ScreenUtilInit(
+    //     designSize: Size(360, 690),
+    //     child: MaterialApp.router(
+    //       debugShowCheckedModeBanner: false,
+    //       routerConfig: AppRouter.router,
+    //       title: 'Quran App',
+    //       theme: ThemeData(
+    //         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    //         useMaterial3: true,
+    //       ),
 
         ),
-
-      ),
-
-
-    );
+      ));
+     
   }
 }
